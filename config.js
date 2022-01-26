36bebc4db6f7d8577e4e8c0be29fd0f14e1b31ca
dapabotz <98033687+dapabotz@users.noreply.github.com>
2022-01-26 18:28:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const os = process
 global.DeveloperMode = 'false' //true Or false
-global.owner = ['6285157072227', '6285157072227'] || os.env["6285727492435"]// Put your number here
-global.linkGC = ['https://chat.whatsapp.com/ImqvWVYNJJW7rhZPHDpTwF:']
-global.mods = ['6285157072227', '6285157072227'] // Want some help?
-global.prems = ['6285157072227', '6285157072227'] // Premium user has unlimited limit
+global.owner = ['16065179419', '16065179419'] || os.env["16065179419"]// Put your number here
+global.linkGC = ['https://chat.whatsapp.com/']
+global.mods = ['16065179419', '16065179419'] // Want some help?
+global.prems = ['16065179419', '16065179419'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Sunscribe Hanif Farhan N'
+global.packname = 'YANZ GANZ🍭🤙'
 global.author = 'JAN LUPA DONATE'
 
 global.multiplier = 69 // The higher, The harder levelup
